317d336251df2a8358e85ca3079ca163862d0e8b
Dodul01 <105786357+Dodul01@users.noreply.github.com>
2022-10-28 13:10:44 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-zzdmaj
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-zzdmaj)
---
+++ src/Nav.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+const Nav = () => {
+  return(
+    <nav>
+      <h1>Logo</h1>
+      <div className="logo-container">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+export default Nav;
