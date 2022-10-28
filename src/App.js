4f8cd447070d21f8fe10d575ce9dc7a88e99e26a
Dodul01 <105786357+Dodul01@users.noreply.github.com>
2022-10-28 21:12:57 +0600

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import "./style.css";
 
 import Nav from './Nav';
@@ -8,9 +9,18 @@ import About from './About';
 export default function App() {
   return (
     <div>
+      <Router>
       <Nav />
-      <Home />
-      <About/>
+      <Switch>
+        <Route>
+          <Home />
+        </Route>
+        <Route>
+          <About/>
+        </Route>
+      </Switch>
+      
+      </Router>
     </div>
   );
 }
