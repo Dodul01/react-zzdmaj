7a96bd286615634cc662cae694d0218bef2bf173
Dodul01 <105786357+Dodul01@users.noreply.github.com>
2022-10-28 18:54:35 +0600

    2 files modified, 2 added

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+const About = () => {
+  return(
+    <div className="about-section">
+      <h1>This is our about section...</h1>
+    </div>
+  )
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 import "./style.css";
 
 import Nav from './Nav';
+import Home from './Home';
+import About from './About';
 
 export default function App() {
   return (
     <div>
       <Nav />
+      <Home />
+      <About/>
     </div>
   );
 }
